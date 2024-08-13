84d55bf11bd95aab592c15e0b0ee63a0d96f0c8f
Travexx <Bwillesta@gmail.com>
2024-08-13 12:27:34 -0700

    09 Solutions addded

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const stringCleared = string
+        .toLowerCase()
+        .split ('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    const reversedString = stringCleared.split('').reverse().join('');
+    return stringCleared == reversedString;
 };
 
 // Do not edit below this line
